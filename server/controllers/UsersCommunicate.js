752746e452671a6f21a122515c89146b2fbc51b2
IvanVojnic <ivojnic44@gmail.com>
2022-08-22 22:32:03 +0300

    smth

--- server/controllers/UsersCommunicate.js
+++ server/controllers/UsersCommunicate.js
@@ -1,26 +1,10 @@
-import User from '../models/user.js';
+import User from "../models/user.js";
 
-import {response} from "express";
-
-export const findUser = (req, res, next) => {
-    req.on("data", email => {
-        User.findOne({where: {email: "data"}})
-            .then(email => {
-                User.findOne({
-                    where:
-                        {
-                            email: "data",
-                            id: "userId"
-                        }
-                }).then(users => {
-                    console.log(users);
-                    let dataUser = users;
-                })
-                if (!email) return console.log("User not found");
-            }).catch(err => console.log(err))
-    });
-    res.on("end", () =>{
-        //response.json(dataUser);
-    });
+export const sendUsers = (req,res,next) => {
+    User.findAll({raw:true})
+        .then(dbUsers => {
+            if (dbUsers) {
+                return res.status(200).json(dbUsers);
+            }
+        })
 }
-
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,8 @@
 import express from 'express';
+
 import { signup, login, isAuth } from '../controllers/auth.js';
-//import {findUser} from "../controllers/UsersCommunicate";
-//import {findUser} from "../controllers/UsersCommunicate";
-//import {findUser} from "../controllers/UsersCommunicate"
+
+import {sendUsers} from '../controllers/UsersCommunicate.js';
 
 const router = express.Router();
 
@@ -10,18 +10,17 @@ router.post('/login', login);
 
 router.post('/signup', signup);
 
-//router.post('/findUser', findUser);
-
 router.get('/private', isAuth);
 
 router.get('/public', (req, res, next) => {
     res.status(200).json({ message: "here is your public resource" });
 });
 
+router.get('/addFriend', sendUsers);
+
 // will match any other path
 router.use('/', (req, res, next) => {
     res.status(404).json({error : "page not found"});
 });
-
 
 export default router;
