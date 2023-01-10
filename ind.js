780804ab54c313464b46b9529d41926bed6cd784
Zavr22 <Mkulik2407@gmail.com>
2023-01-11 00:43:16 +0300

    not working validation for form

--- ind.js
+++ ind.js
@@ -18,6 +18,30 @@ window.onload = (event) => {
     let circle1Coords = circle1.getBoundingClientRect()
     let circle2Coords = circle2.getBoundingClientRect()
     let line1Coords = line1.getBoundingClientRect()
+    let form = document.getElementById("form1")
+    let emailField = document.getElementById("email").value
+    let instField = document.getElementById("inst").value
+
+    const email = emailField.nodeValue
+    const inst = instField.nodeValue
+
+
+
+   form.addEventListener('submit', function (event) {
+
+        if(!emailField && !instField) {
+            console.log("err")
+            event.preventDefault();
+        }
+        else {
+            event.submit()
+            console.log("заебись")
+
+        }
+
+    });
+
+
 
     //line1.style.height =
 };
